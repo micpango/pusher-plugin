27971e0d9cbec6417c89fd4292621900a8ec177c
Michael Pangopoulos <michael.pangopoulos@finn.no>
2012-04-22 19:27:57 +0200

    Upgraded to grails 2.0.3

--- PusherGrailsPlugin.groovy
+++ PusherGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class PusherGrailsPlugin {
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0.0 > *"
+    def grailsVersion = "2.0.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [rest: '0.7']
     // resources that are excluded from plugin packaging
